52c0e3341f027d7cc465893b2f107ab031fc42bb
James 9251 <james35113@gmail.com>
2020-12-27 19:56:17 +0000

    Add solution for puzzle 2, day 6 and, tidied solution for puzzle 1

--- day6/puzzles.py
+++ day6/puzzles.py
@@ -1,17 +1,29 @@
+import string
+
 # Opening the file containing questionnaire information
 with open('input.txt', 'r') as f:
     answer_file = f.read().splitlines()
 
-group_answers = []
-new_answers = set()
+# Lists of the two group answers in sets, under the different conditions
+intersection_group_answers = []
+union_group_answers = []
+new_intersection_group = set(list(string.ascii_lowercase))
+new_union_group = set()
+
 for answer in answer_file:
     if answer == '':
-        group_answers.append(new_answers)
-        new_answers = set()
+        intersection_group_answers.append(new_intersection_group)
+        union_group_answers.append(new_union_group)
+        new_intersection_group = set(list(string.ascii_lowercase))
+        new_union_group = set()
         continue
-    new_answers.update(set(list(answer)))
-if len(new_answers) > 0:
-    group_answers.append(new_answers)
+    new_intersection_group = new_intersection_group.intersection(set(list(answer)))
+    new_union_group.update(set(list(answer)))
+
+intersection_group_answers.append(new_intersection_group)
+union_group_answers.append(new_union_group)
     
-count = sum(len(x) for x in group_answers)
-print('Total = {}'.format(count))
+union_count = sum(len(x) for x in union_group_answers)
+print('Part 1 total = {}'.format(union_count))
+intersection_count = sum(len(x) for x in intersection_group_answers)
+print('Part 2 total = {}'.format(intersection_count))
